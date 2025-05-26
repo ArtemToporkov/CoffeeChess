dea16a0796cbbac3576a48c4f76cafcd4a66e554
ArtemToporkov <toporkovartem259@gmail.com>
2025-05-26 18:28:53 +0500

    game logic implementing started 🚋

---
+++ CoffeeChess.Web/Controllers/GameController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeChess.Web.Controllers;
+
+public class GameController : Controller
+{
+    public IActionResult Game()
+    {
+        if (Request.Headers.XRequestedWith == "XMLHttpRequest")
+        {
+            return PartialView("_Game");
+        }
+
+        return View();
+    }
+}
---
+++ CoffeeChess.Web/Models/ViewModels/GameViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CoffeeChess.Web.Models;
+
+public class GameViewModel
+{
+    public int Rating { get; set; }
+
+    public string OpponentName { get; set; }
+    public int OpponentRating { get; set; }
+}
