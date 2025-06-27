000b18d7cdce4b072fdc370e0fe6e89403f2b796
ArtemToporkov <toporkovartem259@gmail.com>
2025-06-27 18:21:57 +0500

    refactor: move game logic to GameManager 🎮

---
+++ CoffeeChess.Web/wwwroot/js/game/GameManager.js
@@ -0,0 +1,96 @@
+﻿import { HistoryManager } from "./HistoryManager.js";
+import { TimersManager } from "./TimersManager.js";
+
+export class GameManager {
+    shouldReturnToLive;
+    historyManager;
+    timersManager;
+    game;
+    gameId;
+    board;
+    connection;
+    isWhite;
+    isWhiteTurn;
+    
+    constructor(connection, gameId, isWhite, totalMillisecondsLeft) {
+        this.gameId = gameId;
+        this.connection = connection;
+        
+        this.game = new Chess();
+        this.board = ChessBoard('myBoard', this.#getConfig());
+        this.historyManager = new HistoryManager(this.game, this.board);
+        this.timersManager = new TimersManager(totalMillisecondsLeft);
+        this.timersManager.start();
+        
+        this.shouldReturnToLive = false;
+        this.isWhiteTurn = true;
+        this.isWhite = isWhite; 
+    }
+    
+    updateGameState(pgn, newWhiteMillisecondsLeft, newBlackMillisecondsLeft) {
+        this.game.load_pgn(pgn);
+
+        $('#myBoard .piece-417db, body > img.piece-417db').stop(true, true);
+        this.board.position(this.game.fen());
+
+        this.isWhiteTurn = this.game.turn() === 'w';
+
+        this.timersManager.updateTimers(newWhiteMillisecondsLeft, newBlackMillisecondsLeft, this.isWhiteTurn);
+        if (this.timersManager.whiteMillisecondsLeft < 0 
+            || this.timersManager.whiteMillisecondsLeft < 0) {
+            // TODO: implement losing game after time runs out
+        }
+
+        this.historyManager.resetHistoryEvents();
+    }
+    
+    #isMyTurn() {
+        return (this.isWhite && this.isWhiteTurn) 
+            || (!this.isWhite && !this.isWhiteTurn)
+    }
+    
+    #getConfig() {
+        const onDragStart = (source, piece, position, orientation) => {
+            if ((!this.#isMyTurn() || this.game.game_over()) 
+                && this.historyManager.currentPly === this.game.history().length)
+                return false;
+        }
+
+        const onDrop = (source, target) => {
+            if (this.historyManager.currentPly !== this.game.history().length) {
+                this.shouldReturnToLive = true;
+                return;
+            }
+
+            const move = this.game.move({
+                from: source,
+                to: target,
+                promotion: 'q'
+            });
+
+            if (move === null)
+                return 'snapback';
+
+            this.connection.invoke("MakeMove", this.gameId, move.from, move.to, move.promotion);
+        }
+
+        const onSnapEnd = () => {
+            if (this.shouldReturnToLive) {
+                $('.history-selected').removeClass('history-selected');
+                this.shouldReturnToLive = false;
+                this.historyManager.setLastMoveToSelected();
+            }
+            this.board.position(this.game.fen());
+        }
+
+        return {
+            draggable: true,
+            position: 'start',
+            onDragStart: onDragStart,
+            onDrop: onDrop,
+            onSnapEnd: onSnapEnd,
+            pieceTheme: '/img/chesspieces/{piece}.png',
+            snapbackSpeed: 250
+        };
+    }
+}
--- CoffeeChess.Web/wwwroot/js/game/board-setup.js
+++ CoffeeChess.Web/wwwroot/js/game/board-setup.js
@@ -1,5 +1,4 @@
-import { HistoryManager } from "./HistoryManager.js";
-import { TimersManager } from "./TimersManager.js";
+import { GameManager } from "./GameManager.js";
 
 $(document).ready(() => {
     const connection = new signalR.HubConnectionBuilder()
@@ -10,55 +9,12 @@ $(document).ready(() => {
     const pathParts = window.location.pathname.split('/');
     const gameId = pathParts[pathParts.length - 1];
     
-    let board = null;
-    const game = new Chess();
-    let historyManager = null;
-    let shouldReturnToLive = false;
-
-    function onDragStart(source, piece, position, orientation) {
-        if ((!isMyTurn || game.game_over()) && historyManager.currentPly === game.history().length) 
-            return false;
-    }
-
-    function onDrop(source, target) {
-        if (historyManager.currentPly !== game.history().length) {
-            shouldReturnToLive = true;
-            return;
-        }
-        
-        const move = game.move({
-            from: source,
-            to: target,
-            promotion: 'q'
-        });
-
-        if (move === null) 
-            return 'snapback';
-        
-        connection.invoke("MakeMove", gameId, move.from, move.to, move.promotion);
-    }
-    
-    function onSnapEnd() {
-        if (shouldReturnToLive) {
-            isMyTurn = (isWhite && isWhiteTurn) || (!isWhite && !isWhiteTurn);
-            $('.history-selected').removeClass('history-selected');
-            shouldReturnToLive = false;
-            historyManager.setLastMoveToSelected();
-        }
-        board.position(game.fen());
-    }
-    
-    const config = {
-        draggable: true,
-        position: 'start',
-        onDragStart: onDragStart,
-        onDrop: onDrop,
-        onSnapEnd: onSnapEnd,
-        pieceTheme: '/img/chesspieces/{piece}.png',
-        snapbackSpeed: 250
-    };
-    board = Chessboard('myBoard', config);
-    historyManager = new HistoryManager(game, board);
+    const gameManager = new GameManager(
+        connection, 
+        gameId, 
+        localStorage.getItem('isWhite') === "true",
+        localStorage.getItem('totalMillisecondsLeft')
+    );
     
     const whitePlayerInfo = JSON.parse(localStorage.getItem('whitePlayerInfo'));
     const blackPlayerInfo = JSON.parse(localStorage.getItem('blackPlayerInfo'));
@@ -68,33 +24,13 @@ $(document).ready(() => {
     $('#whiteRating').text(whitePlayerInfo.rating);
     $('#blackRating').text(blackPlayerInfo.rating);
     
-    const isWhite = localStorage.getItem('isWhite') === "true";
-    let isWhiteTurn = true;
-    let isMyTurn = isWhite;
-    if (!isWhite) {
-        board.flip();
+    if (!gameManager.isWhite) {
+        gameManager.board.flip();
         $('.game-middle-panel').addClass('flipped');
     }
     
-    const totalMillisecondsLeft = localStorage.getItem('totalMillisecondsLeft');
-    const timersManager = new TimersManager(totalMillisecondsLeft);
-    timersManager.start();
-    
     connection.on("MakeMove", (pgn, newWhiteMillisecondsLeft, newBlackMillisecondsLeft) => {
-        game.load_pgn(pgn);
-
-        $('#myBoard .piece-417db, body > img.piece-417db').stop(true, true);
-        board.position(game.fen());
-        
-        isWhiteTurn = game.turn() === 'w';
-        isMyTurn = (isWhite && isWhiteTurn) || (!isWhite && !isWhiteTurn);
-        
-        timersManager.updateTimers(newWhiteMillisecondsLeft, newBlackMillisecondsLeft, isWhiteTurn);
-        if (timersManager.whiteMillisecondsLeft < 0 || timersManager.whiteMillisecondsLeft < 0) {
-            // TODO: implement losing game after time runs out
-        }
-        
-        historyManager.resetHistoryEvents();
+        gameManager.updateGameState(pgn, newWhiteMillisecondsLeft, newBlackMillisecondsLeft);
     });
     
     connection.on("MoveFailed", (errorMessage) => {
