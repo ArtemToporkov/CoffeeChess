eab213f9ccfeb088e325e877c2b5ec20df9d4cfe
ArtemToporkov <toporkovartem259@gmail.com>
2025-06-27 13:14:49 +0500

    refactor: moved history logic to HistoryManager 🧑🏻‍💼

--- CoffeeChess.Web/wwwroot/js/game/board-setup.js
+++ CoffeeChess.Web/wwwroot/js/game/board-setup.js
@@ -1,3 +1,5 @@
+import { HistoryManager } from "./history.js";
+
 $(document).ready(() => {
     const connection = new signalR.HubConnectionBuilder()
         .withUrl("/gameHub")
@@ -9,18 +11,16 @@ $(document).ready(() => {
     
     let board = null;
     const game = new Chess();
-    const historyViewGame = new Chess();
-    let currentPly = 0;
-    let currentMoveIndex = -1;
+    let historyManager = null;
     let shouldReturnToLive = false;
 
     function onDragStart(source, piece, position, orientation) {
-        if (!isMyTurn || game.game_over()) 
+        if ((!isMyTurn || game.game_over()) && historyManager.currentPly === game.history().length) 
             return false;
     }
 
     function onDrop(source, target) {
-        if (currentPly !== game.history().length) {
+        if (historyManager.currentPly !== game.history().length) {
             shouldReturnToLive = true;
             return;
         }
@@ -39,11 +39,10 @@ $(document).ready(() => {
     
     function onSnapEnd() {
         if (shouldReturnToLive) {
-            currentPly = game.history().length;
             isMyTurn = (isWhite && isWhiteTurn) || (!isWhite && !isWhiteTurn);
             $('.history-selected').removeClass('history-selected');
-            setLastMoveToSelected();
             shouldReturnToLive = false;
+            historyManager.setLastMoveToSelected();
         }
         board.position(game.fen());
     }
@@ -58,6 +57,7 @@ $(document).ready(() => {
         snapbackSpeed: 250
     };
     board = Chessboard('myBoard', config);
+    historyManager = new HistoryManager(game, board);
     
     const whitePlayerInfo = JSON.parse(localStorage.getItem('whitePlayerInfo'));
     const blackPlayerInfo = JSON.parse(localStorage.getItem('blackPlayerInfo'));
@@ -109,88 +109,19 @@ $(document).ready(() => {
         $("#blackTimeLeft").text(`${blackMinutesText}:${blackSecondsText}`);
     }
     
-    function updateHistory() {
-        const history = $('#history');
-        history.children().slice(1).remove();
-        const moves = game.history();
-        for (let i = 0; i < moves.length; i += 2) {
-            const whiteMove = moves[i];
-            const blackMove = i + 1 < moves.length ? moves[i + 1] : '';
-            let historyRow = getHistoryRow(i / 2 + 1, whiteMove, blackMove);
-            
-            historyRow.children().eq(1).on('click', () => {
-                undoToPly(i + 1);
-            });
-            if (blackMove !== '') {
-                historyRow.children().eq(2).on('click', () => {
-                   undoToPly(i + 2); 
-                });
-            }
-            
-            history.append(historyRow);
-        }
-        setLastMoveToSelected();
-    }
-    
-    function undoToPly(ply) {
-        $('#myBoard .piece-417db, body > img.piece-417db').stop(true, true);
-        
-        $('.history-selected').removeClass('history-selected');
-        const childNumber = ply % 2 === 0 ? 2 : 1; 
-        const moveNumber = Math.ceil(ply / 2);
-        $('#history').children().eq(moveNumber).children().eq(childNumber).addClass('history-selected');
-        historyViewGame.reset();
-        for (let i = 0; i < ply; i++) {
-            historyViewGame.move(game.history()[i]);
-        }
-        currentPly = ply;
-        isMyTurn = true;
-        board.position(historyViewGame.fen());
-    }
-    
-    function setLastMoveToSelected() {
-        const history = $('#history');
-        const moves = game.history();
-        if (moves.length > 0) {
-            const lastRow = history.children().last();
-            const lastMoveElement = (moves.length % 2 === 0) ? lastRow.children().eq(2) : lastRow.children().eq(1);
-            lastMoveElement.addClass('history-selected');
-        }
-    }
-    
-    function getHistoryRow(number, whiteMove, blackMove) {
-        return $('<div>', {
-            class: 'history-row'
-        }).append(
-            $('<div>', {
-                class: 'history-move-number',
-                text: number,
-            }).css('cursor', 'default'),
-            $('<div>', {
-                class: 'history-move',
-                text: whiteMove
-            }).css('cursor', whiteMove === 'White' ? 'default' : 'pointer'),
-            $('<div>', {
-                class: 'history-move',
-                text: blackMove
-            }).css('cursor', ['', 'Black'].includes(blackMove) ? 'default' : 'pointer')
-        );
-    }
-    
     connection.on("MakeMove", (pgn, newWhiteMillisecondsLeft, newBlackMillisecondsLeft) => {
         game.load_pgn(pgn);
 
         $('#myBoard .piece-417db, body > img.piece-417db').stop(true, true);
         board.position(game.fen());
         
-        currentPly += 1;
         isWhiteTurn = game.turn() === 'w';
         isMyTurn = (isWhite && isWhiteTurn) || (!isWhite && !isWhiteTurn);
         whiteMillisecondsLeft = newWhiteMillisecondsLeft;
         blackMillisecondsLeft = newBlackMillisecondsLeft;
         
         updateTimers();
-        updateHistory();
+        historyManager.resetHistoryEvents();
     });
     
     connection.on("MoveFailed", (errorMessage) => {
@@ -201,24 +132,5 @@ $(document).ready(() => {
         // TODO: raise 500 with errorMessage
     });
     
-    $(document).on('keydown', e => {
-       switch (e.key) {
-           case 'ArrowLeft':
-               if (currentPly > 1) {
-                   undoToPly(currentPly - 1);
-               }
-               break;
-           case 'ArrowRight':
-               if (currentPly < game.history().length) {
-                   undoToPly(currentPly + 1);
-               }
-               break;
-       } 
-    });
-    
-    async function startConnection() {
-        await connection.start();
-    }
-    
-    startConnection();
+    connection.start();
 });
---
+++ CoffeeChess.Web/wwwroot/js/game/history.js
@@ -0,0 +1,95 @@
+﻿export class HistoryManager {
+    #game;
+    #historyViewGame;
+    #board;
+    currentPly = 0;
+    
+    constructor(game, board) {
+        this.#game = game;
+        this.#board = board;
+        this.#historyViewGame = new Chess();
+
+        $(document).on('keydown', e => {
+            switch (e.key) {
+                case 'ArrowLeft':
+                    if (this.currentPly > 1) {
+                        this.#undoToPly(this.currentPly - 1);
+                    }
+                    break;
+                case 'ArrowRight':
+                    if (this.currentPly < this.#game.history().length) {
+                        this.#undoToPly(this.currentPly + 1);
+                    }
+                    break;
+            }
+        });
+    }
+
+    resetHistoryEvents() {
+        this.currentPly = this.#game.history().length;
+        const history = $('#history');
+        history.children().slice(1).remove();
+        const moves = this.#game.history();
+        for (let i = 0; i < moves.length; i += 2) {
+            const whiteMove = moves[i];
+            const blackMove = i + 1 < moves.length ? moves[i + 1] : '';
+            let historyRow = this.#getHistoryRow(i / 2 + 1, whiteMove, blackMove);
+
+            historyRow.children().eq(1).on('click', () => {
+                this.#undoToPly(i + 1);
+            });
+            if (blackMove !== '') {
+                historyRow.children().eq(2).on('click', () => {
+                    this.#undoToPly(i + 2);
+                });
+            }
+
+            history.append(historyRow);
+        }
+        this.setLastMoveToSelected();
+    }
+
+    #undoToPly(ply) {
+        $('#myBoard .piece-417db, body > img.piece-417db').stop(true, true);
+
+        $('.history-selected').removeClass('history-selected');
+        const childNumber = ply % 2 === 0 ? 2 : 1;
+        const moveNumber = Math.ceil(ply / 2);
+        $('#history').children().eq(moveNumber).children().eq(childNumber).addClass('history-selected');
+        this.#historyViewGame.reset();
+        for (let i = 0; i < ply; i++) {
+            this.#historyViewGame.move(this.#game.history()[i]);
+        }
+        this.currentPly = ply;
+        this.#board.position(this.#historyViewGame.fen());
+    }
+
+    setLastMoveToSelected() {
+        const history = $('#history');
+        const moves = this.#game.history();
+        if (moves.length > 0) {
+            const lastRow = history.children().last();
+            const lastMoveElement = (moves.length % 2 === 0) ? lastRow.children().eq(2) : lastRow.children().eq(1);
+            lastMoveElement.addClass('history-selected');
+        }
+    }
+
+    #getHistoryRow(number, whiteMove, blackMove) {
+        return $('<div>', {
+            class: 'history-row'
+        }).append(
+            $('<div>', {
+                class: 'history-move-number',
+                text: number,
+            }).css('cursor', 'default'),
+            $('<div>', {
+                class: 'history-move',
+                text: whiteMove
+            }).css('cursor', whiteMove === 'White' ? 'default' : 'pointer'),
+            $('<div>', {
+                class: 'history-move',
+                text: blackMove
+            }).css('cursor', ['', 'Black'].includes(blackMove) ? 'default' : 'pointer')
+        );
+    }
+}
