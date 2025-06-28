45d5e09c0cf4384ae37a3f4845aa4630ada6fe09
ArtemToporkov <toporkovartem259@gmail.com>
2025-06-28 14:22:08 +0500

    feat: configure resign button 🔳

--- CoffeeChess.Web/wwwroot/js/game/ui.js
+++ CoffeeChess.Web/wwwroot/js/game/ui.js
@@ -11,4 +11,32 @@
         gameManager.board.flip();
         $('.game-middle-panel').addClass('flipped');
     }
+    
+    const $resignDrawButtonsContainer = $('#resignDrawButtonsContainer');
+    const $confirmButtonsContainer = $('#confirmationButtonsContainer');
+    const $confirmButton = $('#confirmButton');
+    const $denyButton = $('#denyButton');
+    const $resignDrawMessage = $('#resignDrawMessage');
+    
+    const turnButtonsBack = () => {
+        $resignDrawMessage.text('');
+        $resignDrawButtonsContainer.css('display', 'flex');
+        $confirmButtonsContainer.css('display', 'none');
+    };
+    
+    $('#resignButton').on('click', () => {
+        $resignDrawMessage.text('Are you sure?');
+        $resignDrawButtonsContainer.css('display', 'none');
+        $confirmButtonsContainer.css('display', 'flex');
+        
+        $confirmButton.text('Yes');
+        $confirmButton.on('click', () => {
+            // TODO: implement resignation
+        });
+        
+        $denyButton.text('No');
+        $denyButton.on('click', () => {
+            turnButtonsBack();
+        });
+    })
 }
