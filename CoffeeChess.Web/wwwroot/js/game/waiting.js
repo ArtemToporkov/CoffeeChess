31a7ba41d6bc1f33a90daedaa0869dc5d32c483f
ArtemToporkov <toporkovartem259@gmail.com>
2025-07-09 18:58:52 +0500

    refactor: move <script> code to js file 🍳
    move js code from GameWaiting.cshtml to waiting.js

---
+++ CoffeeChess.Web/wwwroot/js/game/waiting.js
@@ -0,0 +1,32 @@
+﻿import { GameHubMethods } from "./enums/GameHubMethods.js";
+
+$(document).ready(() => {
+    
+    const connection = new signalR.HubConnectionBuilder()
+        .withUrl("/gameHub")
+        .configureLogging(signalR.LogLevel.Information)
+        .build();
+    
+    connection.on(GameHubMethods.GameStarted, (gameId, isWhite, whitePlayerInfo,
+                                               blackPlayerInfo, totalMillisecondsLeft) => {
+        localStorage.setItem("totalMillisecondsLeft", totalMillisecondsLeft);
+        localStorage.setItem("isWhite", isWhite);
+        localStorage.setItem("whitePlayerInfo", JSON.stringify(whitePlayerInfo));
+        localStorage.setItem("blackPlayerInfo", JSON.stringify(blackPlayerInfo));
+        window.location.href = `/Game/Play/${gameId}`;
+    });
+
+    const gameSettings = $("#gameSettings").data('gameSettings');
+    console.log(gameSettings);
+    
+    async function startSignalR() {
+        try {
+            await connection.start();
+            await connection.invoke(GameHubMethods.CreateOrJoinGame, gameSettings);
+        } catch (err) {
+            console.error(err.toString());
+        }
+    }
+    
+    startSignalR();
+});
