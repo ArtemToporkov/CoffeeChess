7adaf67809bacf84270fa6bf8337c2325859974d
ArtemToporkov <toporkovartem259@gmail.com>
2025-08-11 19:15:31 +0500

    feat: make audio play 🌵

---
+++ CoffeeChess.Web/wwwroot/js/music-player.js
@@ -0,0 +1,27 @@
+﻿$(document).ready(() => {
+    let currentSongIdx = 0;
+    const $musicPlayer = $('#musicPlayer');
+    const $author = $('#songAuthor');
+    const $title = $('#songTitle');
+    const $cover = $('#songCover');
+    loadTrack(currentSongIdx);
+    
+    function loadTrack(songIdx) {
+        const track = playlist[songIdx];
+        $author.text(track.author);
+        $title.text(track.title);
+        $musicPlayer.attr("src", track.audioSrc);
+        $musicPlayer[0].play();
+        $cover.attr("src", track.coverSrc);
+    }
+});
+
+const playlist = Object.freeze([
+        {
+            title: "U Can Get It",
+            author: "Proleter",
+            audioSrc: "../tracks/Proleter-U-Can-Get-It/Proleter-U-Can-Get-It.mp3",
+            coverSrc: "../tracks/Proleter-U-Can-Get-It/Proleter-U-Can-Get-It.jpg"
+        }
+    ]
+)
