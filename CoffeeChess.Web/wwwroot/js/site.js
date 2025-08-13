1474aab178a5e0ef6c4fc081bd2da61ba9e23571
ArtemToporkov <toporkovartem259@gmail.com>
2025-08-13 22:30:19 +0500

    feat: add welcome-play ajax transition 🎴

--- CoffeeChess.Web/wwwroot/js/site.js
+++ CoffeeChess.Web/wwwroot/js/site.js
@@ -1,1 +1,17 @@
-﻿
+﻿$(document).ready(function () {
+    const $mainContainer = $('main[role="main"]');
+    
+    function loadContent(url) {
+        $.get(url, data => {
+            $mainContainer.html(data);
+        }).fail(() => {
+           throw new Error('Could not load content.'); 
+        });
+    }
+    
+    $(document).on('click', '.ajax-nav-link', e => {
+        e.preventDefault();
+        const url = $(e.currentTarget).data('url');
+        loadContent(url);
+    });
+})
