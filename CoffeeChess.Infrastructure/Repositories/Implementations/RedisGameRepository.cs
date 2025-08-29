3d96a9083a15e65fbbf6bcccfc14eb6a976a4488
ArtemToporkov <toporkovartem259@gmail.com>
2025-08-29 18:13:26 +0500

    refactor: use transactions in game repository 🦖

--- CoffeeChess.Infrastructure/Repositories/Implementations/RedisGameRepository.cs
+++ CoffeeChess.Infrastructure/Repositories/Implementations/RedisGameRepository.cs
@@ -44,23 +44,24 @@ public class RedisGameRepository(
         var metadata = gamePersistenceModel.StaticMetadata
             .Concat(gamePersistenceModel.MetadataThatCanUpdate)
             .ToArray();
-        
-        // TODO: perform in a transaction
-
-        await _database.HashSetAsync(
+        var transaction = _database.CreateTransaction();
+        _ = transaction.HashSetAsync(
             GetMetadataKey(game.GameId), metadata);
-        await _database.HashSetAsync(
+        _ = transaction.HashSetAsync(
             GetPositionsForThreefoldKey(game.GameId), gamePersistenceModel.PositionsForThreefold);
-        await _database.ListRightPushAsync(
-            GetMovesHistoryKey(game.GameId), gamePersistenceModel.MovesHistory);
+        if (gamePersistenceModel.MovesHistory.Length > 0)
+            _ = transaction.ListRightPushAsync(
+                GetMovesHistoryKey(game.GameId), gamePersistenceModel.MovesHistory);
+        await transaction.ExecuteAsync();
     }
 
     public async Task DeleteAsync(Game game, CancellationToken cancellationToken = default)
     {
-        // TODO: perform in a transaction
-        await _database.KeyDeleteAsync(GetMetadataKey(game.GameId));
-        await _database.KeyDeleteAsync(GetMovesHistoryKey(game.GameId));
-        await _database.KeyDeleteAsync(GetPositionsForThreefoldKey(game.GameId));
+        var transaction = _database.CreateTransaction();
+        _ = transaction.KeyDeleteAsync(GetMetadataKey(game.GameId));
+        _ = transaction.KeyDeleteAsync(GetMovesHistoryKey(game.GameId));
+        _ = transaction.KeyDeleteAsync(GetPositionsForThreefoldKey(game.GameId));
+        await transaction.ExecuteAsync();
     }
 
     public async Task SaveChangesAsync(Game game, CancellationToken cancellationToken = default)
@@ -69,24 +70,25 @@ public class RedisGameRepository(
         var positionsKey = GetPositionsForThreefoldKey(game.GameId);
         var movesHistoryKey = GetMovesHistoryKey(game.GameId);
 
-        // TODO: perform in a transaction
+        var transaction = _database.CreateTransaction();
         using var scope = serviceProvider.CreateScope();
         var mediator = scope.ServiceProvider.GetRequiredService<IMediator>();
         foreach (var @event in game.DomainEvents)
         {
             if (@event is MoveMade moveMade)
             {
-                await _database.ListRightPushAsync(
+                _ = transaction.ListRightPushAsync(
                     movesHistoryKey, JsonSerializer.Serialize(moveMade.MoveInfo, GameSerializationOptions));
                 // TODO: check if it's possible to not overwrite the whole dictionary
-                await _database.KeyDeleteAsync(positionsKey);
-                await _database.HashSetAsync(positionsKey, gamePersistenceModel.PositionsForThreefold);
+                _ = transaction.KeyDeleteAsync(positionsKey);
+                _ = transaction.HashSetAsync(positionsKey, gamePersistenceModel.PositionsForThreefold);
             }
 
             await mediator.Publish(@event, cancellationToken);
         }
 
-        await _database.HashSetAsync(GetMetadataKey(game.GameId), gamePersistenceModel.MetadataThatCanUpdate);
+        _ = transaction.HashSetAsync(GetMetadataKey(game.GameId), gamePersistenceModel.MetadataThatCanUpdate);
+        await transaction.ExecuteAsync();
         game.ClearDomainEvents();
     }
 
